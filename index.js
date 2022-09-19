a94bbe25af4537ca4de9da74fd1aed56e15fcde4
matejadjeric <113903240+matejadjeric@users.noreply.github.com>
2022-09-19 14:37:08 +0200

    1 file modified

---
+++ index.js
@@ -0,0 +1,4 @@
+let a = prompt("Unesite prvi broj");
+let b = prompt("Unesite drugi broj");                                                                     
+let c = Number(a)+Number(b);
+console.log("Zbir brojeva je: " + c);
